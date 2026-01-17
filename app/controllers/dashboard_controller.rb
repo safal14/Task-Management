487944ade0f2039e5f1db207f9b9082bf3ea79b1
Safal Acharya <acharyasafal51@gmail.com>
2026-01-17 12:22:25 +0545

    Create: Created dashboard after login

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,7 @@
+class DashboardController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    # Temporary – just to see something
+    @greeting = "Hello #{current_user.first_name}!"
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  get "dashboard/index"
   devise_for :users
-root "posts#index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+root to: "dashboard#index"
+  get "dashboard", to: "dashboard#index"  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
