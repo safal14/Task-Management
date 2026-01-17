2a6a135e288a94aeeb6d3ec1d310bb89b91efa1f
Safal Acharya <acharyasafal51@gmail.com>
2026-01-17 15:11:38 +0545

    Add: added  pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,21 @@
 class ApplicationController < ActionController::Base
+  include Pundit::Authorization
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+  # Very important — catches unauthorized access
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  private
+
+  def user_not_authorized
+    flash[:alert] = "You are not allowed to do that."
+    redirect_back(fallback_location: dashboard_path)
+  end
+
+  # Makes current_user available to policies
+  def pundit_user
+    current_user
+  end
 end
