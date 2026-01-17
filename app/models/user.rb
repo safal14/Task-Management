675f7b5c34c91eed42c0cb9db9d60b3a256eb6b4
Safal Acharya <acharyasafal51@gmail.com>
2026-01-17 12:02:36 +0545

    Add: added user with role enum

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # Include default devise modules.
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum :role, {
+    admin:   0,
+    manager: 1,
+    member:  2
+  }
+
+  validates :first_name, :last_name, presence: true
+
+  # We'll add associations later
 end
